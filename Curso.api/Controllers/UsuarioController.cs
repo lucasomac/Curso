c8e4b87cb944126aeed939a543f2845529cd1310
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-12-15 10:13:14 -0300

    Initial commit

---
+++ Curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        // GET
+        public IActionResult Logar()
+        {
+            return View();
+        }
+    }
+}
